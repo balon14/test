b5a01319bcb54b4cf1ac33aa878711611dcbdb53
balon14 <117996775+balon14@users.noreply.github.com>
2023-05-22 18:19:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,76 +1,20 @@
-from transformers import YolosImageProcessor, YolosForObjectDetection
-import torch
-import io
 import streamlit as st
-from PIL import Image
-import numpy as np
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from sbert_punc_case_ru import SbertPuncCase
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
+import string
 
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    return EfficientNetB0(weights='imagenet')
 
-
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
-
-
-def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-
-
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
-
-
-model = load_model()
-
-
-st.title('Новая улучшенная классификации изображений в облаке Streamlit')
-img = load_image()
-result = st.button('Распознать изображение')
-if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write('**Результаты распознавания:**')
-    print_predictions(preds)
-    
-
-url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/American_Eskimo_Dog.jpg/800px-American_Eskimo_Dog.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-
-model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
-image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
-
-inputs = image_processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-
-# model predicts bounding boxes and corresponding COCO classes
-logits = outputs.logits
-bboxes = outputs.pred_boxes
-
-
-# print results
-target_sizes = torch.tensor([image.size[::-1]])
-results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
-        f"Detected {model.config.id2label[label.item()]} with confidence "
-        f"{round(score.item(), 3)} at location {box}"
-    )
+def lang_detection(input_text):
+    try:
+        detected_text = detect(input_text)
+        if detected_text != 'ru':
+            st.error("Язык введенного текста не русский! Попробуйте ввести \
+                текст еще раз.")
+            return "Язык введенного текста не русский! " + \
+                "Попробуйте ввести текст еще раз."
+        else:
+            lowercase_text = input_text.lower()
+            text_to_punctuate = lowercase_text.translate(str.maketrans('', '',
+                                                         string.punctuation))
