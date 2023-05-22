0b12976666db53a04428e8db6fc3bcd5fa972d07
balon14 <117996775+balon14@users.noreply.github.com>
2023-05-22 23:18:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from streamlit_punctuate import lang_detection
 
 
 def test_lang_detection():
-    assert lang_detection("the weather is great in Moscow") == \
+    assert lang_detection("Dmitry") == \
         "Язык введенного текста не русский! Попробуйте ввести текст еще раз."
-    assert lang_detection("") == \
+    assert lang_detection(",,,,....") == \
         "Невозможно определить язык! Попробуйте ввести текст еще раз."
