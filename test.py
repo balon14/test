83ca0eb9b77ff04a55c45dd78cfce2dad27520ed
balon14 <117996775+balon14@users.noreply.github.com>
2023-05-22 23:18:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-import streamlit as st
-from sbert_punc_case_ru import SbertPuncCase
-from langdetect import detect
-from langdetect.lang_detect_exception import LangDetectException
-import string
+from streamlit_punctuate import lang_detection
 
 
-
-def lang_detection(input_text):
-    try:
-        detected_text = detect(input_text)
-        if detected_text != 'ru':
-            st.error("Язык введенного текста не русский! Попробуйте ввести \
-                текст еще раз.")
-            return "Язык введенного текста не русский! " + \
-                "Попробуйте ввести текст еще раз."
-        else:
-            lowercase_text = input_text.lower()
-            text_to_punctuate = lowercase_text.translate(str.maketrans('', '',
-                                                         string.punctuation))
+def test_lang_detection():
+    assert lang_detection("the weather is great in Moscow") == \
+        "Язык введенного текста не русский! Попробуйте ввести текст еще раз."
+    assert lang_detection("") == \
+        "Невозможно определить язык! Попробуйте ввести текст еще раз."
