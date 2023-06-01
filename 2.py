374f7eba51b1a985af69953417c240a9f1a9c423
balon14 <117996775+balon14@users.noreply.github.com>
2023-06-01 16:31:45 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,31 @@
+import io
+import streamlit as st
+from PIL import Image
+import numpy as np
+from transformers import YolosFeatureExtractor, YolosForObjectDetection
+import requests
 
+
+name = st.text_input("Enter your name", "")
+st.write(f"Hello {name}!")
+
+
+def load_image():
+    """Создание формы для загрузки изображения"""
+    # Форма для загрузки изображения средствами Streamlit
+    uploaded_file = st.file_uploader(
+        label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        # Получение загруженного изображения
+        image_data = uploaded_file.getvalue()
+        # Показ загруженного изображения на Web-странице средствами Streamlit
+        st.image(image_data)
+        # Возврат изображения в формате PIL
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+# Выводим заголовок страницы средствами Streamlit     
+st.title('Классификация изображений')
+# Вызываем функцию создания формы загрузки изображения
+img = load_image()
